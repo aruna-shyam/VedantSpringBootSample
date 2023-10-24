f975d206c5a4c6751ddad77e0614081560c57ee8
aruna-shyam <arunarsha1412@gmail.com>
2023-10-24 22:10:52 +0530

    Upload Project

---
+++ src/main/java/com/palle/VedantSampleApplication.java
@@ -0,0 +1,13 @@
+package com.palle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedantSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VedantSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palle/VedantSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VedantSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
